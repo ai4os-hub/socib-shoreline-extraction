21a898190008529c75084b7ffe54f99b298c37d4
OliverJosep <josep.oliverr@gmail.com>
2025-12-04 11:16:36 +0100

    feat: Refactor predict function to handle different response formats

--- socib_shoreline_identification/api.py
+++ socib_shoreline_identification/api.py
@@ -24,24 +24,17 @@ module [2].
 [2]: https://github.com/ai4os-hub/ai4os-demo-app
 """
 
-import ast
-import base64
-import json
 import logging
-import math
-import mimetypes
-from pathlib import Path
-from random import random
 import os
-import shutil
-import tempfile
-import time
+from io import BytesIO
+from pathlib import Path
 
+import cv2
 from webargs import fields, validate
 
-from . import config, misc
-
 from socib_shoreline_identification.app.predictor import ShorelinePredictor
+
+from . import config
 
 # set up logging
 logger = logging.getLogger(__name__)
@@ -70,6 +63,7 @@ def get_metadata():
         logger.error("Error collecting metadata: %s", err, exc_info=True)
         raise  # Reraise the exception after log
 
+
 def get_predict_args():
     """
     TODO: add more dtypes
@@ -91,11 +85,9 @@ def get_predict_args():
         "accept": fields.Str(
             required=False,
             missing="application/json",
-            validate=validate.OneOf(
-                ["application/json", "application/zip", "image/*"]
-            ),
+            validate=validate.OneOf(["application/json", "image/*"]),
             metadata={
-                "description": "Select the desired response MIME type.",
+                "description": "Select the desired response format.",
             },
         ),
     }
@@ -103,40 +95,58 @@ def get_predict_args():
 
 # @_catch_error
 def predict(**kwargs):
-    """
-    
-    """
+    """ """
     print("Received kwargs:", kwargs)
 
     image_file = kwargs.get("file")
     if image_file is None:
         raise ValueError("No image file provided in the 'file' argument.")
-    
+
     image_path = image_file.filename
     print("Image path:", image_path)
-    
     # Testing only for rectified images, 3 classes
     is_rectified = True
 
     path = "rectified" if is_rectified else "oblique"
-    model_weight_path = os.path.abspath(os.path.join(os.getcwd(), f"models/{path}_best_model.pth"))
+    model_weight_path = os.path.abspath(
+        os.path.join(os.getcwd(), f"models/{path}_best_model.pth")
+    )
     print("Model weight path:", model_weight_path)
 
     model = "DeepLabV3"
     num_classes = 3 if is_rectified else 2
 
     predictor = ShorelinePredictor(model, model_weight_path, num_classes)
-    
+
     landward_pixel_pred = 1 if is_rectified else 0
     seaward_pixel_pred = 2 if is_rectified else 1
 
-    output = predictor.predict(image_path, patch_size=(256, 256), stride=(128, 128), landward_pixel_pred=landward_pixel_pred, seaward_pixel_pred=seaward_pixel_pred)
+    output = predictor.predict(
+        image_path,
+        patch_size=(256, 256),
+        stride=(128, 128),
+        landward_pixel_pred=landward_pixel_pred,
+        seaward_pixel_pred=seaward_pixel_pred,
+    )
     print("Prediction completed.")
     print("Output keys:", output.keys())
     print("Output predicted_image shape:", output["predicted_image"].shape)
 
-    # TODO: handle different accept types
-    return output["predicted_image"]
+    if kwargs.get("accept") == "image/*":
+        output["predicted_image"] = cv2.cvtColor(
+            output["predicted_image"], cv2.COLOR_BGR2RGB
+        )
+        _, buffer = cv2.imencode(".png", output["predicted_image"])
+        image_buffer = BytesIO(buffer)
+
+        return image_buffer
+
+    if kwargs.get("accept") == "application/json":
+        return output["shoreline_coords"]
+
+    # return message if no valid accept type is found
+    return {"message": "No valid accept type found."}
+
 
 # # Schema to validate the `predict()` output if accept field is "application/json"
 # schema = {
